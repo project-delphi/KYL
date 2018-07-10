7ce4a3dd60d828821213142517ab543f75fb1fe7
Alexis Michell <35441705+alexmichellcc@users.noreply.github.com>
2018-07-09 20:23:29 -0500

    gui
    added crowdsale contracts

---
+++ Contracts/KYLCrowdsale.sol
@@ -0,0 +1,224 @@
+pragma solidity ^0.4.22;
+
+import './KYLToken.sol';
+
+contract Crowdsale {
+    using SafeMath for uint256;       
+
+    // The token being sold
+    MintableToken public token;
+
+    // start and end block where investments are allowed (both inclusive)
+    uint256 public startBlock;
+    uint256 public endBlock;
+
+    // address where funds are collected
+    address public wallet;
+
+    // how many token units a buyer gets per wei
+    uint256 public rate;
+
+    // amount of raised money in wei
+    uint256 public weiRaised;
+
+    /**
+    * event for token purchase logging
+    * @param purchaser who paid for the tokens
+    * @param beneficiary who got the tokens
+    * @param value weis paid for purchase
+    * @param amount amount of tokens purchased
+    */
+    event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
+
+    constructor (uint256 _startBlock, uint256 _endBlock, uint256 _rate, address _wallet) public {
+        //require(_startBlock >= block.number, "Actual Block is higher");
+        require(_endBlock >= _startBlock, "EB is not higher than SB");
+        require(_rate > 0, "Rate must be positive");
+        require(_wallet != 0x0, "Funds wallet cannot be 0x0");
+
+        token = createTokenContract();
+        startBlock = _startBlock;
+        endBlock = _endBlock;
+        rate = _rate;
+        wallet = _wallet;
+    }
+
+    // creates the token to be sold.
+    // override this method to have crowdsale of a specific mintable token.
+    function createTokenContract() internal returns (MintableToken) {
+        return new MintableToken();
+    }
+
+    // fallback function can be used to buy tokens
+    function () public payable {
+        buyTokens(msg.sender);
+    }
+
+    // low level token purchase function
+    function buyTokens(address beneficiary) public payable {
+        require(beneficiary != 0x0, "IS ZEOR");
+        require(validPurchase(), "INVALID P");
+
+        uint256 weiAmount = msg.value;
+
+        // calculate token amount to be created
+        uint256 tokens = weiAmount.mul(rate);
+        // update state
+        weiRaised = weiRaised.add(weiAmount);
+
+        token.mint(beneficiary, tokens);
+        emit TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
+        forwardFunds();
+    }
+
+    // send ether to the fund collection wallet
+    // override to create custom fund forwarding mechanisms
+    function forwardFunds() internal {
+        wallet.transfer(msg.value);
+    }
+
+    // @return true if the transaction can buy tokens
+    function validPurchase() internal view returns (bool) {
+        uint256 current = block.number;
+        bool withinPeriod = current >= startBlock && current <= endBlock;
+        bool nonZeroPurchase = msg.value != 0;
+        return withinPeriod && nonZeroPurchase;
+    }
+
+    // @return true if crowdsale event has ended
+    function hasEnded() public view returns (bool) {
+        return block.number > endBlock;
+    }
+    
+    function CURRENT() public view returns(uint){
+        return block.number;
+    }
+}
+
+contract WhitelistedCrowdsale is Crowdsale, Ownable {
+    mapping (address => bool) whitelist;
+
+    function addToWhitelist(address buyer) public onlyOwner {
+        require(buyer != 0x0, "Invalid Address");
+        whitelist[buyer] = true;
+    }
+
+    function isWhitelisted(address buyer) public view returns (bool) {
+        return whitelist[buyer];
+    }
+
+    function validPurchase() internal view returns (bool) {
+        bool isValid = (!hasEnded()) && isWhitelisted(msg.sender);
+        return super.validPurchase() || isValid;
+    }
+}
+
+contract CappedCrowdsale is Crowdsale {
+    uint256 public cap;
+
+    constructor(uint256 _cap) public {
+        require(_cap > 0);
+        cap = _cap;
+    }
+    
+    function validPurchase() internal constant returns (bool) {
+        bool withinCap = weiRaised.add(msg.value) <= cap;
+        return super.validPurchase() && withinCap;
+    }
+
+    function hasEnded() public constant returns (bool) {
+        bool capReached = weiRaised >= cap;
+        return super.hasEnded() || capReached;
+    }
+}
+
+contract KYLCrowdsale is WhitelistedCrowdsale, CappedCrowdsale{
+    //custom rate for each whitelisted buyer
+    mapping(address => uint256) buyerRate;
+     
+    event PreferentialRateChange(address indexed buyer, uint256 rate);
+    event InitialRateChange(uint256 rate);
+    event EndRateChange(uint256 rate);
+    
+    uint256 preRate;
+    uint256 iniRate;
+    uint256 endRate;
+
+    enum phase{EA, pICO, ICO}
+    phase public stage;
+    
+    constructor(
+        uint256 _startBlock,
+        uint256 _endBlock,
+        uint256 _preRate,
+        uint256 _iniRate,
+        uint256 _endRate,
+        address wallet
+    )
+        public 
+        Crowdsale(_startBlock, _endBlock, _iniRate, wallet)
+        WhitelistedCrowdsale()
+        CappedCrowdsale(100 ether)
+    {
+        require(_iniRate > 0 && _endRate > 0 && _preRate > 0, "Rate is zero");
+        preRate = _preRate;
+        iniRate = _iniRate;
+        endRate = _endRate;
+        stage = phase.EA;
+    }
+
+    /** general functions */
+    function getRate() public view returns(uint256){
+        if (buyerRate[msg.sender] != 0) {
+            return buyerRate[msg.sender];
+        }
+
+        if (isWhitelisted(msg.sender)) {
+            return preRate;
+        }
+
+        uint256 elapsed = block.number - startBlock;
+        uint256 rateRange = iniRate - endRate;
+        uint256 blockRange = endBlock - startBlock;
+        return iniRate.sub(rateRange.mul(elapsed).div(blockRange));
+    }
+
+    /**early access: whitelisting */
+    /** @override */
+    function addToWhitelist(address buyer) public onlyOwner {
+        require(stage == phase.EA, "Early Access Finished");
+        super.addToWhitelist(buyer);
+    }
+    
+    function setPreferentialRate(address buyer, uint256 rate) public onlyOwner{
+        require(stage == phase.EA, "Early access finished");
+        require(isWhitelisted(buyer), "Address not whitelisted");
+        require(rate != 0, "Rate cannot be zero");
+
+        buyerRate[buyer] = rate;
+        emit PreferentialRateChange(buyer, rate);
+    }
+
+    /**preico access */
+
+    function buyTokens(address who) public payable{
+        require(stage != phase.EA, "Cannot buy tokens in actual phase");
+        require(who != 0x0, "Invalid address");
+        require(validPurchase(), "Invalid purchase");
+
+        uint256 value = msg.value;
+        uint256 rate = getRate();
+        uint256 tokens = value.mul(rate);
+
+        weiRaised = weiRaised.add(value);
+
+        token.mint(who, tokens);
+        emit TokenPurchase(msg.sender, who, value, tokens);
+        
+        forwardFunds();
+    }
+
+    function hasEnded() public view returns(bool){
+        return super.hasEnded();
+    }
+}
--- Contracts/KYLToken.sol
+++ Contracts/KYLToken.sol
@@ -1,143 +1,107 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.22;
+
+import "./SafeMath.sol";
 
 contract Ownable {
-    address public owner;
-
+    address public owner;   
     /**
-    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-    * account.
+    * @dev sets the original `owner` of the contract to the sender account.
     */
-    function Ownable() {
+    constructor() public{
         owner = msg.sender;
-    }
-
+    }   
     /**
     * @dev Throws if called by any account other than the owner.
     */
     modifier onlyOwner() {
         require(msg.sender == owner);
         _;
-    }
-
+    }   
     /**
     * @dev Allows the current owner to transfer control of the contract to a newOwner.
     * @param newOwner The address to transfer ownership to.
     */
-    function transferOwnership(address newOwner) onlyOwner {
+    function transferOwnership(address newOwner) onlyOwner public{
         if (newOwner != address(0)) {
-        owner = newOwner;
+            owner = newOwner;
         }
     }
 }
 
 contract Pausable is Ownable {
-  event Pause();
-  event Unpause();
+    event Pause();
+    event Unpause();
 
-  bool public paused = false;
-
-
-  /**
-   * @dev modifier to allow actions only when the contract IS paused
-   */
-  modifier whenNotPaused() {
-    require(!paused);
-    _;
-  }
-
-  /**
-   * @dev modifier to allow actions only when the contract IS NOT paused
-   */
-  modifier whenPaused {
-    require(paused);
-    _;
-  }
-
-  /**
-   * @dev called by the owner to pause, triggers stopped state
-   */
-  function pause() onlyOwner whenNotPaused returns (bool) {
-    paused = true;
-    Pause();
-    return true;
-  }
-
-  /**
-   * @dev called by the owner to unpause, returns to normal state
-   */
-  function unpause() onlyOwner whenPaused returns (bool) {
-    paused = false;
-    Unpause();
-    return true;
-  }
+    bool public paused = false;
+    /**
+    * @dev modifier to allow actions only when the contract IS paused
+    */
+    modifier whenNotPaused() {
+        require(!paused);
+        _;
+    }
+    /**
+    * @dev modifier to allow actions only when the contract IS NOT paused
+    */
+    modifier whenPaused {
+        require(paused);
+        _;
+    }
+    /**
+    * @dev called by the owner to pause, triggers stopped state
+    */
+    function pause() onlyOwner whenNotPaused public returns (bool) {
+        paused = true;
+        emit Pause();
+        return true;
+    }
+    /**
+    * @dev called by the owner to unpause, returns to normal state
+    */
+    function unpause() onlyOwner whenPaused public returns (bool) {
+        paused = false;
+        emit Unpause();
+        return true;
+    }
 }
 
 contract ERC20Basic {
-  uint256 public totalSupply;
-  function balanceOf(address who) constant returns (uint256);
-  function transfer(address to, uint256 value) returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
+    uint256 public totalSupply;
+    function balanceOf(address who) public view returns (uint256);
+    function transfer(address to, uint256 value) public returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
 contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) constant returns (uint256);
-  function transferFrom(address from, address to, uint256 value) returns (bool);
-  function approve(address spender, uint256 value) returns (bool);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
-}
-
-library SafeMath {
-  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
-
-  function div(uint256 a, uint256 b) internal constant returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
-
-  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  function add(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
+    function allowance(address owner, address spender) public view returns (uint256);
+    function transferFrom(address from, address to, uint256 value) public returns (bool);
+    function approve(address spender, uint256 value) public returns (bool);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
 contract BasicToken is ERC20Basic {
-  using SafeMath for uint256;
+    using SafeMath for uint256;
 
-  mapping(address => uint256) balances;
-
-  /**
-  * @dev transfer token for a specified address
-  * @param _to The address to transfer to.
-  * @param _value The amount to be transferred.
-  */
-  function transfer(address _to, uint256 _value) returns (bool) {
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    Transfer(msg.sender, _to, _value);
-    return true;
-  }
-
-  /**
-  * @dev Gets the balance of the specified address.
-  * @param _owner The address to query the the balance of.
-  * @return An uint256 representing the amount owned by the passed address.
-  */
-  function balanceOf(address _owner) constant returns (uint256 balance) {
-    return balances[_owner];
-  }
-
+    mapping(address => uint256) balances;
+    /**
+    * @dev transfer token for a specified address
+    * @param _to The address to transfer to.
+    * @param _value The amount to be transferred.
+    */
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+    /**
+    * @dev Gets the balance of the specified address.
+    * @param _owner The address to query the the balance of.
+    * @return An uint256 representing the amount owned by the passed address.
+    */
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
 }
 
 contract StandardToken is ERC20, BasicToken {
@@ -182,7 +146,7 @@ contract StandardToken is ERC20, BasicToken {
     * @param _spender address The address which will spend the funds.
     * @return A uint256 specifing the amount of tokens still avaible for the spender.
     */
-    function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
         return allowed[_owner][_spender];
     }
 }
@@ -190,10 +154,6 @@ contract StandardToken is ERC20, BasicToken {
 contract BurnableToken is StandardToken {
     event Burn(address indexed burner, uint256 value);
 
-    /**
-     * @dev Burns a specified amount of tokens.
-     * @param _value The amount of tokens to burn.
-     */
     function burn(uint256 _value) public {
         require(_value > 0);
 
@@ -226,12 +186,6 @@ contract MintableToken is StandardToken, Ownable {
         _;
     }
 
-    /**
-    * @dev Function to mint tokens
-    * @param _to The address that will recieve the minted tokens.
-    * @param _amount The amount of tokens to mint.
-    * @return A boolean that indicates if the operation was successful.
-    */
     function mint(address _to, uint256 _amount) onlyOwner canMint public returns (bool) {
         totalSupply = totalSupply.add(_amount);
         balances[_to] = balances[_to].add(_amount);
@@ -239,10 +193,6 @@ contract MintableToken is StandardToken, Ownable {
         return true;
     }
 
-    /**
-    * @dev Function to stop minting new tokens.
-    * @return True if the operation was successful.
-    */
     function finishMinting() onlyOwner public returns (bool) {
         mintingFinished = true;
         emit MintFinished();
@@ -256,7 +206,10 @@ contract KYLToken is BurnableToken, PausableToken, MintableToken {
     uint8 public constant decimals = 18;
 
     function burn(uint256 _value) whenNotPaused public {
-        //From BurnableToken
         super.burn(_value);
+    }
+
+    function mint(address _to, uint256 _amount) whenNotPaused public returns (bool) {
+        return super.mint(_to, _amount);
     }
 }
